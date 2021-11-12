afedfc7d5709647f6b8fb5aad473502041715be6
tsw0015 <tsw0015@mix.wvu.edu>
2021-11-12 15:43:18 -0500

    Cleaned up code and add commenting to explain how the code works

--- computer-vision/test.m
+++ computer-vision/test.m
@@ -1,51 +1,43 @@
 clear, clc, close all
 
-cam = webcam('/dev/video2');
-%preview(cam)
-while(true)
-    img = snapshot(cam);
-    % imshow(img)
-    % figure;
+filter_threshold = 0.43;   
 
+%assing camera to the port the webcam is (use webcamlist command in command
+%window to list out the difference webcam devices connected to your device
+%and assign accordingly
+cam = webcam('/dev/video3');
+
+%continually update the picture using while loop (ctrl + C in command
+%window to kill the process)
+while(true)
+    %take an image
+    img = snapshot(cam);
+    
+    %split into individual Red Green and Blue Matricies
     [R,G,B] = imsplit(img);
     R = double(R);
     G = double(G);
     B = double(B);
-    filter_threshold = 0.43;
-
-    % imshow(cat(3, R, zeros(size(G)), zeros(size(B))));
-    % figure;
-    %
-    % imshow(cat(3, zeros(size(R)), G, zeros(size(B))));
-    % figure;
-    %
-    % imshow(cat(3, zeros(size(R)), zeros(size(G)), B));
-    % figure;
    
-
+    %create mask for filtering white
     white_filter = uint16(R)+uint16(G)+uint16(B);
     white_filter = double(white_filter);
-    % for i = 1:size(white_filter,1)
-    %     for j = 1:size(white_filter,2)
-    %         if white_filter(i,j) > 500
-    %             white_filter(i,j)= 0
-    %         end
-    %     end clear('cam')
-    % end
-    % white_filter = white_filter < 375;
-    % white_filter = uint8(white_filter);
+   
+    %Find percentages of what color each pizel is made up of
     R_filter = R./white_filter;
     B_filter = B./white_filter;
     G_filter = G./white_filter;
 
+    %Use the thresholding to mask the image
     R_filter = R_filter > filter_threshold;
     G_filter = G_filter > filter_threshold;
     B_filter = B_filter > filter_threshold;
 
-
+    %create new image
     new_image(:,:,1) = uint16(R).*uint16(R_filter);
     new_image(:,:,2) = uint16(G).*uint16(G_filter);
     new_image(:,:,3) = uint16(B).*uint16(B_filter);
-
+    
+    %display the new image
     imshow(uint8(new_image))
 end
